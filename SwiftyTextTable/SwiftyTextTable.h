e2dfe1c9cd8480cecc4b44011f08fedf998def27
Scott Hoyt <scott@wildonion.io>
2016-02-03 21:03:46 -0800

    Initial Commit

---
+++ SwiftyTextTable/SwiftyTextTable.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyTextTable.h
+//  SwiftyTextTable
+//
+//  Created by Scott Hoyt on 2/3/16.
+//  Copyright © 2016 Scott Hoyt. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftyTextTable.
+FOUNDATION_EXPORT double SwiftyTextTableVersionNumber;
+
+//! Project version string for SwiftyTextTable.
+FOUNDATION_EXPORT const unsigned char SwiftyTextTableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyTextTable/PublicHeader.h>
+
+
