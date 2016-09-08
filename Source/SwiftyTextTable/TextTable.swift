aa2c18cf2e7516e3247852a2b12646ec69b083bb
Scott Hoyt <scott@wildonion.io>
2016-09-07 18:09:15 -0700

    Cleanup

--- Source/SwiftyTextTable/TextTable.swift
+++ Source/SwiftyTextTable/TextTable.swift
@@ -22,12 +22,6 @@ public protocol TextTableObject {
     var tableValues: [CustomStringConvertible] { get }
 }
 
-//extension String: CustomStringConvertible {
-//    public var description: String {
-//        return self
-//    }
-//}
-
 private extension String {
     func withPadding(_ count: Int) -> String {
         let length = characters.count
