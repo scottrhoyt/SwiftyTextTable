453fe751b547ccb2e1bd009f7a0b41669417c24c
JP Simard <jp@jpsim.com>
2018-09-13 12:39:19 -0700

    Fix SwiftPM manifest and release 0.8.2

--- Package.swift
+++ Package.swift
@@ -3,6 +3,9 @@ import PackageDescription
 
 let package = Package(
   name: "SwiftyTextTable",
+  products: [
+    .library(name: "SwiftyTextTable", targets: ["SwiftyTextTable"])
+  ],
   targets: [
     .target(name: "SwiftyTextTable"),
     .testTarget(name: "SwiftyTextTableTests", dependencies: [
--- SwiftyTextTable.podspec
+++ SwiftyTextTable.podspec
@@ -16,7 +16,7 @@ Pod::Spec.new do |s|
   #
 
   s.name         = "SwiftyTextTable"
-  s.version      = "0.8.1"
+  s.version      = "0.8.2"
   s.summary      = "A lightweight Swift library for generating text tables."
 
   # This description is used to generate tags and improve search results.
